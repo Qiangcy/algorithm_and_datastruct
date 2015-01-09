e9012ede0c33473476b0678f98d5a00aff56c43d
YongHao <christopherwuy@gmail.com>
2015-01-09 23:47:51 +0800

    heap sort: Add output and fix heap_sort

--- sort/heap_sort.cpp
+++ sort/heap_sort.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void heap_sertion_sort(int *a, int length);
 void heap_adjust(int *a, int i, int length);
 void swap( int &a, int &b );
+void output(int *a, int length);
 int main()
 {
     int length = 0;
@@ -16,9 +17,18 @@ int main()
     heap_sertion_sort(a, length);
 }
 
+void output(int *a, int length)
+{
+    for(int i=0; i<length; ++i)
+        cout<<a[i]<<' ';
+    cout<<endl;
+}
+
 void heap_sertion_sort(int *a, int length)
 {
-    for(int i=(length-1)/2; i>0; i--) {
+    for(int i=(length-1)/2-1; i>-1; i--) {
+        cout<<"heap_adjust : i"<<i<<endl;
+        output(a, length);
         heap_adjust(a, i, length);
     }
 }
@@ -27,18 +37,23 @@ void heap_adjust(int *a, int i, int length)
 {
     int temp = 0;
     temp = a[i];
-    for(int j=(i+1)*2-1; j<length-1; j=i*2+1) {
-        if(a[2*j+1] > a[j]) {
-            swap(a[2*j+1], a[j]);
+    for(int j=(i+1)*2-1; j<length-1; j=j*2+1) {
+        if(a[j] > a[i]) {
+            swap(a[j], a[i]);
             //a[j] = a[2*j+1];
             //a[2*j+1] = temp;
         }
-        if(a[2*j+2] > a[j]) {
-            swap(a[2*j+2], a[j]);
+        if(a[j+1] > a[i]) {
+            swap(a[j+1], a[i]);
             //a[j] = a[2*j+2];
             //a[2*j+2] = temp;
         }
+        cout<<"i"<<i<<"  j : "<<j<<endl;
+        //cout<<"loop"<<endl;
+        //char c;
+        //cin>>c;
     }
+    cout<<"end"<<endl;
 }
 
 void swap( int &a, int &b )
