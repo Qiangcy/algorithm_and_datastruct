62e4dfd884b01475644ba65b1934d7876c53c0cc
YongHao <christopherwuy@gmail.com>
2015-01-19 23:26:57 +0800

    add void protype

--- graph/adj_graph.h
+++ graph/adj_graph.h
@@ -10,7 +10,9 @@ typedef struct vertex_node{
     struct edge_node *first_edge;
 }Adj_list[10];
 
-type struct adj_graph{
+typedef struct adj_graph{
     Adj_list adj_list;
     int num_of_vertexs, num_of_edges;
 }Adj_Graph;
+
+void InitGraph();
