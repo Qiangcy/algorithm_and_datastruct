c0638efcd3b3e2eefe9852d4e30bb2f70cd978d6
YongHao <christopherwuy@gmail.com>
2015-02-03 18:35:33 +0800

    my_sprintf: Add format parse

--- algorithms/my_sprintf.c
+++ algorithms/my_sprintf.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-void my_sprintf(double val, int precision);
+void my_sprintf(double val, int precision, char format);
 using namespace std;
 int main()
 {
@@ -12,11 +12,39 @@ int main()
     cout<<"cin precision"<<endl;
     int precision = 0;
     cin>>precision;
-    my_sprintf(val, precision);
+    cout<<"cin format(e,f,g)"<<endl;
+    char format = 'f';
+    cin>>format;
+    my_sprintf(val, precision, format);
 }
 
-void my_sprintf(double val, int precision)
+void my_sprintf(double val, int precision, char format)
 {
+    int mysign = 1, exponent = 0;
+    if(format == 'e' || format == 'g') {
+        if(val >= 10) {
+            mysign = 1;
+            while(val >= 10) {
+                val /= 10;
+                ++exponent;
+            }
+        }else if(val < 1) {
+            mysign = 0;
+            while(val < 1) {
+                val *= 10;
+                ++exponent;
+            }
+        }
+
+        if(format == 'g') {
+            if(exponent < -4)
+                format = 'e';
+            else
+                format = 'f';
+        }
+    }
+    printf(" \n after e :  %f exponent is  %d  \n" , val, exponent);
+
     double float_val, temp, back_up = val;
     int len = 0;
     int base = 10, up_to_integer = 0;
@@ -30,8 +58,12 @@ void my_sprintf(double val, int precision)
     cout<<"float_val after (int) is "<<float_val<<endl;
     for(int i=0; i<precision; ++i)
         float_val *= 10;
-    //cout<<"round is : "<<round(float_val)<<endl;
+
+    printf("before round is %f \n", float_val);
+    printf("after  round is %f \n", round(float_val));
+
     temp = float_val = round(float_val);
+    printf("temp %f \n log 10 %f \n log 10 %f\n", temp, log10(temp), floor(log10(temp))+1);
     while(temp != 0) {
         temp = (int)temp / 10;
         ++i;
@@ -78,7 +110,7 @@ void my_sprintf(double val, int precision)
             j = (int)float_val%base;
             float_val = (int)float_val/base;
             buf[i++] = digits[j];
-           //cout<<" and j is "<<j<<' ';
+            //cout<<" and j is "<<j<<' ';
         }
         buf[i] = '\0';
 
@@ -90,7 +122,42 @@ void my_sprintf(double val, int precision)
             j++;
         }
     }
+
+    len = strlen(buf);
+    if(format == 'e') {
+        buf[len++] = 'e';
+
+        if(mysign == 1)
+            buf[len++] = '+';
+        else
+            buf[len++] = '-';
+
+        if(exponent < 100)
+            buf[len++] = '0';
+        if(exponent < 10) {
+            buf[len++] = '0';
+            buf[len++] = exponent + '0';
+            buf[len] = '\0';
+        }else {
+            printf("e >= 10");
+            i = len;
+            while(exponent != 0) {
+                j = exponent%base;
+                exponent = exponent/base;
+                buf[i++] = digits[j];
+            }
+            buf[i] = '\0';
+
+            j = len;
+            while(--i > j) {
+                char tmp = buf[j];
+                buf[j] = buf[i];
+                buf[i] = tmp;
+                j++;
+            }
+        }
+
+    }
     printf("\nvalue %f using sprintf", back_up);
     cout<<endl<<"****result is  "<<buf<<"**********"<<endl;
-
 }
