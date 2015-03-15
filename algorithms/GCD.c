851d02c6a83aa106a463aabc564e00c2c6723c0d
YongHao <christopherwuy@gmail.com>
2015-03-15 20:08:14 +0800

    Linux: Add anwsers.

--- algorithms/GCD.c
+++ algorithms/GCD.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define N 40
+int a[N];
 
 int GCD(int a, int b)
 {
@@ -70,18 +73,66 @@ void print_numbers(void)
     }
 }
 
+void diamod(int num, char graph)
+{
+    if(num%2 == 0) {//num & 1
+        printf("wrong\n");
+        return;
+    }
+    int half, i;
+    half = num/2;
+    for(i=0; i<=half; ++i) {
+    }
 
+}
+
+void gen_random(int upper_bound)
+{
+    int i;
+    for (i = 0; i < N; i++)
+        a[i] = rand() % upper_bound;
+}
 int main(void)
 {
-    int a, b, c;
-    scanf("%d, %d", &a, &b);
-    printf("GCD result is %d\n", GCD(a, b));
-    printf("W_GCD result is %d\n", GCD(a, b));
-    scanf("%d", &c);
-    printf("Fibonacci result is %d\n", Fibonacci(c));
-    printf("w_Fibonacci result is %d\n", w_Fibonacci(c));
-    printf("\n\n Count %d\n", count());
-    printf("\n\n factorial %d\n", factorial(10));
-    printf("\n\n printf_number \n");
-    print_numbers();
+    int i, histogram[10] = {};
+    gen_random(10);
+    for (i = 0; i < N; i++)
+        ++histogram[a[i]];
+
+    int j, sum;
+    for(i=0; i<=9; i++)
+        printf("%d ", i);
+    printf("\n");
+    for(i=0; i<=9; i++)
+        printf("%d ", histogram[i]);
+    printf("\n");
+    do {
+        sum = 0;
+        for(i=0; i<=9; i++)
+            sum += histogram[i];
+        for(j=0; j<=9; ++j) {
+            if(histogram[j]!=0) {
+                printf("* ");
+                --histogram[j];
+            }else
+                printf("  ");
+        }
+        printf("\n");
+    } while(sum != 0);
+    return 0;
 }
+
+//int main(void)
+//{
+//    int a, b, c;
+//    scanf("%d, %d", &a, &b);
+//    printf("GCD result is %d\n", GCD(a, b));
+//    printf("W_GCD result is %d\n", GCD(a, b));
+//    scanf("%d", &c);
+//    printf("Fibonacci result is %d\n", Fibonacci(c));
+//    printf("w_Fibonacci result is %d\n", w_Fibonacci(c));
+//    printf("\n\n Count %d\n", count());
+//    printf("\n\n factorial %d\n", factorial(10));
+//    printf("\n\n printf_number \n");
+//    print_numbers();
+//}
