c7e147ca9d0c365ae52f7ec090daebbe85058979
YongHao <christopherwuy@gmail.com>
2015-03-17 15:59:55 +0800

    Linux: Add Rational.

--- algorithms/GCD.c
+++ algorithms/GCD.c
@@ -3,12 +3,64 @@
 #define N 40
 int a[N];
 
+struct Ragtional {
+    int numerator;
+    int denominator;
+};
+
 int GCD(int a, int b)
 {
     if(a%b == 0)
         return b;
     else
         return GCD(b, a%b);
+}
+
+struct Ragtional make_rational(int numerator, int denominator)
+{
+    struct Ragtional a;
+    a.numerator = numerator;
+    a.denominator = denominator;
+    return a;
+}
+
+struct Ragtional add_rational(struct Ragtional a, struct Ragtional b)//need to fix -
+{
+    struct Ragtional result;
+    int max, gcm;
+    max = GCD(a.denominator, b.denominator);
+    gcm = (a.denominator*b.denominator) / max;
+    printf("gcm is %d\n", gcm);
+    result.denominator = gcm;
+    result.numerator = (a.numerator*gcm/a.denominator) + (b.numerator*gcm/b.denominator);
+    printf("numerator is %d\n", result.numerator);
+    return result;
+}
+
+struct Ragtional mul_rational(struct Ragtional a, struct Ragtional b)
+{
+    struct Ragtional result;
+    result.numerator = a.numerator * b.numerator;
+    result.denominator = a.denominator * b.denominator;
+    return result;
+}
+
+struct Ragtional div_rational(struct Ragtional a, struct Ragtional b)
+{
+    struct Ragtional trans;
+    trans.numerator =  b.denominator;
+    trans.denominator = b.numerator;
+    return mul_rational(a, trans);
+}
+
+void print_rational(struct Ragtional a)
+{
+    int max;
+    max = GCD(a.numerator, a.denominator);
+    if(a.denominator/max == 1)
+        printf("%d\n", a.numerator/max);
+    else
+        printf("%d/%d\n", a.numerator/max, a.denominator/max);
 }
 
 int w_GCD(int a, int b)
@@ -92,9 +144,17 @@ void gen_random(int upper_bound)
     for (i = 0; i < N; i++)
         a[i] = rand() % upper_bound;
 }
+
 int main(void)
 {
     int i, histogram[10] = {};
+    struct Ragtional aa = make_rational(2, 3);
+    struct Ragtional bb = make_rational(-1, 8);
+    print_rational(add_rational(aa, bb));
+    print_rational(mul_rational(aa, bb));
+    print_rational(div_rational(aa, bb));
+
+
     gen_random(10);
     for (i = 0; i < N; i++)
         ++histogram[a[i]];
