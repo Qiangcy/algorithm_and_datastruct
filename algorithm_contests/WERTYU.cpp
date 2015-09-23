bf531cc3cd220117daea554402178a8ea11f3c7d
YongHao Hu <christopherwuy@gmail.com>
2015-09-23 16:22:11 +0800

    WERTYU

---
+++ algorithm_contests/WERTYU.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    char a[]="`1234567890-=`QWERTYUIOP[]\\ASDFGHJKLLLL;'ZXCVBNM,./";
+    char c;
+    int i;
+    while((c=getchar()) !=EOF) {
+        for(i=0; a[i]&&a[i]!=c; ++i)
+            if(!a[i])
+                putchar(c);
+            else
+                putchar(a[i-1]);
+    }
+    return 0;
+}
