c69f17157b8bd4f26d947e1bedff0fb3a097ca73
YongHao Hu <christopherwuy@gmail.com>
2015-09-26 16:19:21 +0800

    Fix: Score ac now.

--- algorithm_contests/score.cpp
+++ algorithm_contests/score.cpp
@@ -1,4 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        char *str=(char*)malloc(100);
+        scanf("%s", str);
+        int sum=0;
+        for(int i=0; str[i]; ++i) {
+            for(int k=1; str[i]=='O'; ++k) {
+                sum+=k;
+                ++i;
+            }
+        }
+        printf("%d\n", sum);
+    }
+}
+
+/*
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 int main()
 {
@@ -14,6 +36,8 @@ int main()
                 ++i;
             }
         }
-        cout<<sum;
+        printf("%d\n", sum);
+        //        cout<<sum<<endl;
     }
 }
+*/
