761e0e1d3ba80934b6023536f4f585cd4829fe70
YongHao Hu <christopherwuy@gmail.com>
2015-10-13 16:11:51 +0800

    RepeatingDecimal: Sharp the code.

--- algorithm_contests/RepeatingDecimals.cpp
+++ algorithm_contests/RepeatingDecimals.cpp
@@ -9,8 +9,6 @@ int is_substr(vector<int> &vec, int vec_begin, int start)
 {
     if(start+1 >= max_size-start)
         return 0;
-    //    cout<<"start inside is "<<start<<endl;
-    //    cout<<"vec begin is "<<vec_begin<<endl;
     for(int i=vec_begin; i<start; ++i) {
         for(int n=1; (start-vec_begin)*n+i<vec.size(); ++n) {
             if(vec[i] != vec[(start-vec_begin)*n+i])
@@ -22,7 +20,6 @@ int is_substr(vector<int> &vec, int vec_begin, int start)
 
 int print(vector<int> &vec, int start, int end, int num_of_digits)
 {
-    //    cout<<"start is "<<start<<endl;
     if(num_of_digits != 0) {
         for(int i=0; i<end; ++i) {
             if(i == start)
@@ -41,10 +38,7 @@ int is_digital(vector<int> &vec, int start)
 {
     int num_of_digits = 0, is_repeat = 0;
     for(int i=start+1; i<vec.size(); ++i) {
-        //            cout<<"start is "<<start<<" i Is "<<i<<endl;
         if(vec[start] == vec[i]) {
-            //            if(vec.size()%i != 0)
-            //                continue;
             is_repeat = is_substr(vec, start, i);
             if(is_repeat != 0) {
                 num_of_digits = i - start;
@@ -67,7 +61,6 @@ int main()
             vec.push_back(num_of_digits);
             a = a * 10 % b;
         }
-        vector<int>::iterator it = vec.begin();
         cout<<origin_a<<"/"<<b<<" = "<<origin_a/b<<".";
         num_of_digits = 0;
         for(int i=0; i<vec.size(); ++i) {
