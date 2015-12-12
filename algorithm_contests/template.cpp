d060ca7432fa5511c0b4862215c0f6bfebb21866
YongHao Hu <christopherwuy@gmail.com>
2015-12-12 12:48:11 +0800

    template.

---
+++ algorithm_contests/template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+#include <string>
+using namespace std;
+bool cmp(int i, int j) { return i<j; }
+
+int main(void)
+{
+    return 0;
+}
