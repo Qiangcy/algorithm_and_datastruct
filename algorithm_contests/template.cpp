65a535f9bc40ed16d3af00cdf8c34336c76ed494
YongHao Hu <christopherwuy@gmail.com>
2015-12-24 11:29:17 +0800

    improve template.

--- algorithm_contests/template.cpp
+++ algorithm_contests/template.cpp
@@ -10,12 +10,11 @@ bool cmp(int i, int j) { return i<j; }
 int main(void)
 {
 #ifdef LOCAL_PROJECT
-    freopen ("input3.txt", "r", stdin);
+    freopen ("input.txt", "r", stdin);
     //freopen("a.out","w",stdout); // console output is better (in most cases)
 #else
     // add i/o method of specific testing system
 #endif
 
-    //freopen ("input.txt", "r", stdin);
     return 0;
 }
