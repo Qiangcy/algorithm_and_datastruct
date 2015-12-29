9bdc3643530f2f59a0f8fccd695ebad5ff02eaf8
YongHao Hu <christopherwuy@gmail.com>
2015-12-29 14:35:25 +0800

    remove_duplicates_from_sorted_array new solution.

--- leetcode/remove_duplicates_from_sorted_array.c
+++ leetcode/remove_duplicates_from_sorted_array.c
@@ -31,3 +31,17 @@ int removeDuplicates(int* nums, int numsSize) {
     }
     return i;
 }
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0, j=0;
+        while(j < nums.size()) {
+            nums[i] = nums[j];
+            while(nums[i] == nums[j])
+                j++;
+            ++i;
+        }
+        return i;
+    }
+};
