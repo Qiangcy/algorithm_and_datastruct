1b2edfaac25abb1d1c8b2d6d60fde101ead181f5
YongHao Hu <christopherwuy@gmail.com>
2016-01-04 21:20:19 +0800

    print_subset: Version 2 complete.

--- algorithm_contests/print_subset.cpp
+++ algorithm_contests/print_subset.cpp
@@ -25,13 +25,26 @@ void print_subset_a(vector<int> a, int cur)
     print_subset_a(a, cur+1);
 }
 
+void print_subset_b(vector<int> a, int cur)
+{
+    for(int i=0; i<cur; ++i) {
+        if((cur>>i)&1)
+            cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
 int main(void)
 {
     vector<int> a;
-    a.push_back(1);
     a.push_back(2);
     a.push_back(3);
+    a.push_back(4);
+    a.push_back(5);
     b = vector<int>(a.size(), 0);
     print_subset_a(a, 0);
+    cout<<"***"<<endl;
+    for(int i=0; i<(1<<a.size()); ++i) {
+        print_subset_b(a, i);
+    }
     return 0;
 }
