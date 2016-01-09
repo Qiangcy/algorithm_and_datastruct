0a66a185ecc02b5665c88c34e498a438f0862847
YongHao Hu <christopherwuy@gmail.com>
2016-01-09 17:00:23 +0800

    DroppingBalls: Timelimit.

--- algorithm_contests/DroppingBalls.cpp
+++ algorithm_contests/DroppingBalls.cpp
@@ -9,22 +9,27 @@ bool cmp(int i, int j) { return i<j; }
 
 int main(void)
 {
-   int D, I;
-    cin>>D>>I;
-    int all = (1<<D);
-    vector<int> trees(all, 0);
-    int k = 1, tmp=0;
+    int times;
+    while(scanf("%d", &times) && times!=-1) {
+        for(int i=0; i<times; ++i) {
+            int D, I;
+            cin>>D>>I;
+            int all = (1<<D);
+            vector<int> trees(all, 0);
+            int k = 1, tmp=0;
 
-    for(int j=0; j<I; ++j) {
-        k = 1;
-        for(int i=0; i<D-1; ++i) {
-            tmp = k;
-            k = trees[k]?2*k+1:2*k;
-            trees[tmp] = !trees[tmp];
+            for(int j=0; j<I; ++j) {
+                k = 1;
+                for(int i=0; i<D-1; ++i) {
+                    tmp = k;
+                    k = trees[k]?2*k+1:2*k;
+                    trees[tmp] = !trees[tmp];
+                }
+                //cout<<"a round"<<endl<<endl;
+            }
+            printf("%d\n", k);
+//            cout<<k<<endl;
         }
-        //cout<<"a round"<<endl<<endl;
     }
-    cout<<k<<endl;
-
     return 0;
 }
