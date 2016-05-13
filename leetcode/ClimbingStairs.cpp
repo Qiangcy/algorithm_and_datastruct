2f16c9cdb19549f6a8d9da07b099626fd977d1c1
YongHao Hu <christopherwuy@gmail.com>
2016-05-13 21:29:06 +0800

    ClimbingStairs.cpp

---
+++ leetcode/ClimbingStairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    unordered_map<int, int> mymap;
+    int climbStairs(int n) {
+        if(n==0 || n==1)
+            return 1;
+        if(mymap[n])
+            return mymap[n];
+        mymap[n] =  climbStairs(n-1)+climbStairs(n-2);
+        return mymap[n];
+    }
+};
