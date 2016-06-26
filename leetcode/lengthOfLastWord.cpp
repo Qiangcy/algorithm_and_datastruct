3e793a110f6b3eff1bc260fc93e5254a7f7e54a5
YongHao Hu <christopherwuy@gmail.com>
2016-06-26 09:52:46 +0800

    lengthOfLastWord.cpp

---
+++ leetcode/lengthOfLastWord.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        int lengthOfLastWord(string s) {
+            int res = 0;
+            for(int i=0; i<s.length(); ++i) {
+                if(s[i] == ' ')
+                    res = 0;
+                else
+                    ++res;
+            }
+            int len = s.length();
+            if(len >=2 && s[len-1] == ' ' && s[len-2] != ' ')
+                return s.length()-1;
+            else
+                return res;
+        }
+};
