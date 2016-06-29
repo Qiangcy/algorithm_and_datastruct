7f913ac8ff9a6213df50a5b2cda97e5949fccd79
YongHao Hu <christopherwuy@gmail.com>
2016-06-29 11:40:48 +0800

    hammingWeight.cpp

---
+++ leetcode/hammingWeight.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    public:
+        int hammingWeight(uint32_t n) {
+            int res = 0;
+            for(int i=0; i<32; ++i) {
+                if(n&1)
+                    res++;
+                n>>=1;
+            }
+            return res;
+        }
+};
+
+class Solution {
+    public:
+        int hammingWeight(uint32_t n) {
+            int count = 0;
+
+            while (n) {
+                n &= (n - 1);
+                count++;
+            }
+
+            return count;
+        }
+};
