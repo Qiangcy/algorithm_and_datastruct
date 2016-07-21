f5e99115edae543422acb14744e9da4bf3f9eba1
YongHao Hu <christopherwuy@gmail.com>
2016-07-21 19:14:13 +0800

    Single_Number.cpp

--- leetcode/Single_Number.cpp
+++ leetcode/Single_Number.cpp
@@ -26,3 +26,56 @@ public:
         return appear_once;
     }
 };
+
+public int singleNumber(int[] A) {
+    int ones = 0, twos = 0;
+    for(int i = 0; i < A.length; i++){
+        ones = (ones ^ A[i]) & ~twos;
+        twos = (twos ^ A[i]) & ~ones;
+    }
+    return ones;
+}
+
+int singleNumber(int* nums, int numsSize) {
+    // idea : iterate over all elements in array for every bit, get sum of all 1s,
+    // bits with sum not multiple of 3, are 1's bit in the single occurance element
+    // e.g. , 5,5,5,2 - 101 101 101 010 - 313 - answer is 3%3,1%3,3%3 - 010
+
+    int x, res = 0;
+
+    for(int i = 0; i < sizeof(int)*8; i++){
+        x = 1 << i;
+        int sum = 0;
+        for(int j = 0; j < numsSize; j++){
+            if(x & nums[j]) sum++;
+        }
+
+        if(sum % 3) res = res | x;
+    }
+
+    return res;
+}
+
+
+class Solution {
+    // idea : iterate over all elements in array for every bit, get sum of all 1s,
+    // bits with sum not multiple of 3, are 1's bit in the single occurance element
+    // e.g. , 5,5,5,2 - 101 101 101 010 - 313 - answer is 3%3,1%3,3%3 - 010
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size () == 0)
+            return 0;
+        int res = 0, sum;
+        for(int i=0; i<sizeof(int)*8; ++i) {
+            sum = 0;
+            for(auto it : nums) {
+                if((it>>i) & 1)
+                    sum++;
+            }
+            int x = 1<<i;
+            if(sum % 3)
+                res |= x;
+        }
+        return res;
+    }
+};
